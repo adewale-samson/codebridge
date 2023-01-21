f835ca65c304caa7aced1dbdadd3ba3fb9057697
adewale-samson <mascot4sure@gmail.com>
2023-01-21 03:35:28 +0100

    codebridge mui assessment initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import { createTheme, ThemeProvider } from "@mui/material";
+import Card from './Components/Card'
 
 function App() {
+  const theme = createTheme({
+    typography: {
+      fontFamily: ["Montserrat", "sans-serif"].join(","),
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Card /> 
+      </div>
+    </ThemeProvider>
   );
 }
 
